a0b74ab86e7d51a426233b79308c1042341003ca
gabescu <88588343+gabescu@users.noreply.github.com>
2024-05-24 09:19:02 +0200

    Update Harry_Potter_Model.py

--- NLP-Project/Harry_Potter_Model.py
+++ NLP-Project/Harry_Potter_Model.py
@@ -1,1 +1,8 @@
+import Harry_Potter_Model_2
+from Trainer_code import test_dataset
 
+predictions, labels, _ = Harry_Potter_Model_2.predict(test_dataset)
+
+with open("predictions_and_labels.txt", "w") as file:
+  file.write(predictions)
+  file.write(f"\n{labels}")

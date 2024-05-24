2f9a0e42c1ad212c10d6dafc8296f1b61e507c9c
gabescu <88588343+gabescu@users.noreply.github.com>
2024-05-24 12:27:34 +0200

    Update Harry_Potter_Model.py

--- NLP-Project/Harry_Potter_Model.py
+++ NLP-Project/Harry_Potter_Model.py
@@ -1,8 +1,17 @@
-import Harry_Potter_Model_2
+from transformers import AutoModelForTokenClassification, trainer, Trainer
+import os
 from Trainer_code import test_dataset
+import numpy as np
 
-predictions, labels, _ = Harry_Potter_Model_2.predict(test_dataset)
+cwd = os.getcwd()
+saved_path = f"{cwd[0:-11]}Harry_Potter_Model_2"
 
-with open("predictions_and_labels.txt", "w") as file:
+loaded_model = AutoModelForTokenClassification.from_pretrained(saved_path)
+trainer = Trainer(model = loaded_model)
+
+predictions, labels, _ = trainer.predict(test_dataset)
+predictions = np.argmax(predictions, axis = 2)
+
+with open("predictions and labels.txt", "w") as file:
   file.write(predictions)
-  file.write(f"\n{labels}")
+  file.write(labels)
